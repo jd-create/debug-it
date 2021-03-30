4fe0925b493abf95b7476d186422a08beba97f90
jd-create <jenniferdemeijer@t-online.de>
2021-03-30 16:22:11 +0200

    fixed bugs on debug-it-2.js

--- debug-it-2.js
+++ debug-it-2.js
@@ -1,14 +1,15 @@
 var height = 1.79;
 var weight = 56;
 
-var BMI = weight / (heigth * heigth);
+var BMI = weight / (height * height);
 
 console.log("Your BMI is: " + BMI.toFixed());
 
 if (BMI > 25) {
-  console.log(Depending on your build, you might be overweight");
-} else (BMI < 18) {
+  console.log("Depending on your build, you might be overweight");
+} else if (BMI < 18) {
   console.log("Depending on your build, you might be underweight");
 } else {
   console.log("Looks like you're height / weight ratio is pretty healthy");
 }
+
